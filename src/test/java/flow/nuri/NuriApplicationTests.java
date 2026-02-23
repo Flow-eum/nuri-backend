951d5fd504fa92613581b9ad54d404fbc9a0f67e
LYJ0304 <omlmg0304@naver.com>
2026-02-23 15:18:07 +0900

    Initial Commit

---
+++ src/test/java/flow/nuri/NuriApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.nuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
