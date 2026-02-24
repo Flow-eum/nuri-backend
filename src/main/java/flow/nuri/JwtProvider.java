5105af7c7629cedf4f9689ff73f4390921181d09
LYJ0304 <omlmg0304@naver.com>
2026-02-24 09:18:57 +0900

    feat: jwt 토큰과 쿠키를 이용한 로그인 기능 구현

---
+++ src/main/java/flow/nuri/JwtProvider.java
@@ -0,0 +1,78 @@
+package flow.nuri;
+
+import flow.nuri.auth.domain.UserRoleEnum;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtProvider {
+
+    public static final String AUTHORIZATION_KEY = "auth";
+    public static final String BEARER_PREFIX = "Bearer";
+
+    private final long TOKEN_TIME = 60 * 60 * 1000L;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        if (secretKey == null || secretKey.isEmpty()) {
+            throw new IllegalStateException("Secret key is not set, in JwtProvider");
+        }
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // 토큰 생성
+    public String createToken(String username, UserRoleEnum role) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username)
+                        .claim(AUTHORIZATION_KEY, role)
+                        .setIssuedAt(date)
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+                        .signWith(key, signatureAlgorithm)
+                        .compact();
+
+    }
+
+    public void addJwtToCookie(String token, HttpServletResponse res) {
+
+        token = substringToken(token);
+        Cookie cookie = new Cookie("Authorization", token);
+        cookie.setPath("/");
+        cookie.setSecure(true);
+        cookie.setMaxAge(3600 * 1000);
+
+        res.addCookie(cookie);
+        log.info("쿠키가 response에 추가되었습니다.");
+    }
+
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        log.error("Not Found Token");
+        throw new NullPointerException("Not Found Token");
+    }
+}
